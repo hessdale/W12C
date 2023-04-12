d0fb4e669c68df591054b85216ff089042ec6d03
dale <hessdale96@gmail.com>
2023-04-11 20:45:54 -0600

    added a log out button and commented my code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
+// defining some variables to use in later functions
 let email = document.getElementById(`email`);
 let password = document.getElementById(`password`);
 let login_button = document.getElementById(`button`);
 let insert_html = document.getElementById(`insert`);
-
+//defining success function for axios request
 function successFunction(response) {
+    //inserting html for success
     insert_html.insertAdjacentHTML(`beforebegin`, `<h1>success</h1>`);
+    //setting cookie to have log in toke
     Cookies.set(`loginToken`, `${response.data.token}`);
+    //moving user to home page
     window.location = `/pages/home.html`;
-}
+};
+//defining failure function for axios request
 function failureFunction(response) {
-    insert_html.insertAdjacentHTML(`beforebegin`, `<h1>failed</h1>`);
-}
-
+    insert_html.insertAdjacentHTML(`beforebegin`, `<h1>login failed</h1>`);
+};
+//defining a function to use an event listener on a button
 function login(details) {
+    //getting the values of email and password
     let email_value = email[`value`];
     let password_value = password[`value`];
+    //axios request to post login info and get return of token if valid log in
     axios.request({
         url: `https://reqres.in/api/login`,
         method: `POST`,
@@ -22,8 +29,16 @@ function login(details) {
             email: email_value,
             password: password_value
         }
-
     }).then(successFunction).catch(failureFunction);
 };
-
+//adding a event listener to login button
 button.addEventListener(`click`, login);
+
+let logout_button = document.getElementById(`delete_cookie`);
+
+
+function logout(details) {
+    Cookies.remove(`loginToken`);
+};
+
+logout_button.addEventListener(`click`, logout);
--- js/home.js
+++ js/home.js
@@ -1,21 +1,27 @@
+//defining html variable to inject html into later
 let main_html = document.getElementById(`main`);
+//variable to check login token
 let get_login = Cookies.get(`loginToken`);
-
+//defining success function for axios reques
 function successFunction(response) {
+    //for loop to insert response information
     for (i = 0; i < response.data.data.length; i++) {
         main_html.insertAdjacentHTML(`beforebegin`, `<div><h1>${response.data.data[i].name}</h1><p>Created in year: ${response.data.data[i].year}</p><div style="width: 200px;height: 200px;background-color:${response.data.data[i].color};"></div></div>`)
     }
-}
+};
+//defining failure function for axios request
 function failureFunction(response) {
+    //inserts html to let user know that request failed
     main_html.insertAdjacentHTML(`beforeend`, `<h1>request failed</h1>`)
-}
-
+};
+//if statement to check and see if the users login token is here injects html if its undefined
 if (get_login === undefined) {
     main_html.insertAdjacentHTML(`beforeend`, `<h1>please log in</h1><a href="../index.html">BACK</a>`)
 } else {
+    //if its not undefined then displays welcome message
     main_html.insertAdjacentHTML(`beforeend`, `<h1>Welcome</h1>`);
+    //axios request to get info from db about colors
     axios.request({
         url: `https://reqres.in/api/unknown`
     }).then(successFunction).catch(failureFunction);
-
-}
+};
